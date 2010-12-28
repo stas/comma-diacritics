0ac71c04af37758579eea3075b5e368bc8652295
Stas SUȘCOV <stas@net.utcluj.ro>
2010-12-28 14:28:13 +0200

    First release

---
+++ comma-diacritics.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Plugin Name: Comma Diacritics
+Plugin URI: http://ubuntu.ro
+Description: Searches and replaces the wrong (sedilla) diacritics with the correct ones on data saving
+Version: 0.1
+Author: Stas Sușcov
+Author URI: http://stas.nerd.ro/
+*/
+?>
+<?php
+/*  Copyright 2010  Stas Sușcov <stas@nerd.ro>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
+<?php
+
+function comma_dias_check( $data ) { 
+    if( !is_array( $data ) )
+        return $data;
+    
+    $diacritics = array(
+        'Ş' => 'Ș',
+        'ş' => 'ș',
+        'Ţ' => 'Ț',
+        'ţ' => 'ț'
+    );
+
+    foreach( $diacritics as $wrong => $right ) {
+        $data['post_title'] = str_replace( $wrong, $right, $data['post_title'] );
+        $data['post_excerpt'] = str_replace( $wrong, $right, $data['post_excerpt'] );
+        $data['post_content'] = str_replace( $wrong, $right, $data['post_content'] );
+    }
+    
+    return $data;
+}
+
+add_filter( 'wp_insert_post_data', 'comma_dias_check' );
+?>
---
+++ readme.txt
@@ -0,0 +1,22 @@
+=== Comma Diacritics ===
+Contributors: sushkov
+Tags: comma, diacritics, romanian
+Requires at least: WordPress 2.9
+Tested up to: WordPress 3.0.3
+Stable tag: 0.1
+Donate link: http://stas.nerd.ro/pub/donate/
+
+Searches and replaces the wrong (sedilla) diacritics with the correct ones on data saving
+
+== Description ==
+
+Some people in Romania use wrong diacritics with sedilla. This plugin will replace the wrong letters with correct ones.
+
+== Installation ==
+
+Please follow the [standard installation procedure for WordPress plugins](http://codex.wordpress.org/Managing_Plugins#Installing_Plugins).
+
+== Changelog ==
+
+= 0.1 =
+* First stable release.
