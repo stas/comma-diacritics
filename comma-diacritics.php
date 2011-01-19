d7655e9468d71c85be820e577a13e3434cf70304
Stas SUȘCOV <stas@net.utcluj.ro>
2011-01-19 15:00:16 +0200

    Added support for cats, terms and general options

--- comma-diacritics.php
+++ comma-diacritics.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: Comma Diacritics
-Plugin URI: http://ubuntu.ro
+Plugin Name: Correct Romanian Diacritics
+Plugin URI: https://github.com/stas/comma-diacritics
 Description: Searches and replaces the wrong (sedilla) diacritics with the correct ones on data saving
-Version: 0.1
+Version: 0.2
 Author: Stas Sușcov
 Author URI: http://stas.nerd.ro/
 */
@@ -28,18 +28,51 @@ Author URI: http://stas.nerd.ro/
 ?>
 <?php
 
-define( 'COMMA_DIAS_VERSION', '0.1' );
+define( 'COMMA_DIAS_VERSION', '0.2' );
 
-function comma_dias_check( $data ) { 
-    if( !is_array( $data ) )
-        return $data;
-    
-    $diacritics = array(
+/**
+ * comma_dias_get()
+ *
+ * Just a wrapper to generate the correct array of diacritics that are to be changed
+ * @return Array
+ */
+function comma_dias_get() {
+    return array(
         'Ş' => 'Ș',
         'ş' => 'ș',
         'Ţ' => 'Ț',
         'ţ' => 'ț'
     );
+}
+
+/**
+ * comma_dias_check_string( $data )
+ *
+ * Hook for 'pre_term_{field}' to filter the correct/wrong diacritics
+ * @param String $data, user posted content
+ * @return String
+ */
+function comma_dias_check_string( $data ) { 
+    $diacritics = comma_dias_get();
+    
+    foreach( $diacritics as $wrong => $right )
+        $data = str_replace( $wrong, $right, $data );
+    
+    return $data;
+}
+
+/**
+ * comma_dias_check_array( $data )
+ *
+ * Hook for 'wp_insert_post_data' to filter the correct/wrong diacritics
+ * @param Array $data, user posted content
+ * @return Array
+ */
+function comma_dias_check_array( $data ) { 
+    if( !is_array( $data ) )
+        return $data;
+    
+    $diacritics = comma_dias_get(); 
 
     foreach( $diacritics as $wrong => $right ) {
         $data['post_title'] = str_replace( $wrong, $right, $data['post_title'] );
@@ -50,11 +83,25 @@ function comma_dias_check( $data ) {
     return $data;
 }
 
+/**
+ * comma_dias_enqueues()
+ *
+ * Sets up the enqueues that add the compatibility layer for Windows platform
+ */
 function comma_dias_enqueues() {
+    // Skip if in wp-admin
+    if( defined( 'WP_ADMIN' ) && WP_ADMIN == true )
+        return;
+    
     wp_register_script( 'commadias', plugins_url( '/js/commadias.plugin.js', __FILE__ ), array( 'jquery' ), COMMA_DIAS_VERSION );
     wp_enqueue_script( 'comma_dias_check', plugins_url( '/js/comma_dias_check.js', __FILE__ ), array( 'commadias' ), COMMA_DIAS_VERSION, true );
 }
 
-add_filter( 'wp_insert_post_data', 'comma_dias_check' );
+/* Register the hooks */
+add_filter( 'esc_html', 'comma_dias_check_string' ); // blogname and blogdescription
+add_filter( 'sanitize_title', 'comma_dias_check_string' ); // categories use this
+add_filter( 'pre_term_name', 'comma_dias_check_string' ); // term name use this
+add_filter( 'pre_term_description', 'comma_dias_check_string' ); // term description uses this
+add_filter( 'wp_insert_post_data', 'comma_dias_check_array' ); // post and pages content uses this
 add_action( 'init', 'comma_dias_enqueues' );
 ?>
--- readme.txt
+++ readme.txt
@@ -1,22 +1,37 @@
 === Comma Diacritics ===
 Contributors: sushkov
-Tags: comma, diacritics, romanian
+Tags: comma, diacritics, romanian, cedilla, virgula, diacritice, diacritică, diacritica, română, românia
 Requires at least: WordPress 2.9
 Tested up to: WordPress 3.0.3
-Stable tag: 0.1
+Stable tag: 0.2
 Donate link: http://stas.nerd.ro/pub/donate/
 
 Searches and replaces the wrong (sedilla) diacritics with the correct ones on data saving
+
 
 == Description ==
 
 Some people in Romania use wrong diacritics with sedilla. This plugin will replace the wrong letters with correct ones.
 
+
 == Installation ==
 
 Please follow the [standard installation procedure for WordPress plugins](http://codex.wordpress.org/Managing_Plugins#Installing_Plugins).
 
+
+== Frequently Asked Questions ==
+
+Please report bugs on [plugin page issues tracker](https://github.com/stas/comma-diacritics/issues).
+
+
 == Changelog ==
 
+= 0.2 =
+* Added support for:
+    * Categories
+    * Terms
+    * General Options
+* Removed JavaScript from `wp-admin`
+
 = 0.1 =
-* First stable release.
+* The pilot.
