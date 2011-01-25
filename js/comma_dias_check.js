b6123117670fd91e78cbfb7d44e957914c700598
Stas SUȘCOV <stas@net.utcluj.ro>
2011-01-25 19:18:02 +0200

    Added check for title element

--- js/comma_dias_check.js
+++ js/comma_dias_check.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function() {  
-    if( navigator.platform == "Win32" || navigator.platform == "Win64" )
+    if( navigator.platform == "Win32" || navigator.platform == "Win64" ) {
         jQuery('#wrapper').commaDias();
+        jQuery('title').commaDias();
+    }
 });
